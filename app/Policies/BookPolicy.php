197d3fa1947e0d9778a7af26a7d5d9f3c3271aa0
Jheng <a0960073588@gmail.com>
2023-04-10 18:13:14 +0800

    feat: user update a book

--- app/Policies/BookPolicy.php
+++ app/Policies/BookPolicy.php
@@ -41,7 +41,10 @@ class BookPolicy
      */
     public function update(User $user, Book $book): bool
     {
-        //
+        //只有自己本身可以修改自己的書本
+        //或者管理員才可以擁有所有權限
+        //透過getkey 方式取得User的id
+        return $user->getkey() === $book->user_id || $user->isadmin();
     }
 
     /**
